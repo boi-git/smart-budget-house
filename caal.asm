f5503a161538dad5b9776bb69ba7ac3bad899baf
boi-git <61734988+boi-git@users.noreply.github.com>
2022-06-25 11:33:56 +0800

    Update caal.asm

--- caal.asm
+++ caal.asm
@@ -18,20 +18,20 @@ voiceon :.asciiz "\nLamp On "
 voiceoff:.asciiz "\nLamp off "
 
 
-			.text
-#1.	Print out a message string with a welcome message
-#syscall print string
+.text
 
-addi $t4, $zero, 0
+addi $t4, $zero, 0 #variable for door to store time when it's open #0 for close 1 for open
+addi $t5, $zero, 0#variable for air codition #0 for bad air 1 for good air
+
 	main:
 		addi $t0, $zero, 9  
 		
-		while:
+		while: # while loop and will exit if user enter 0
 
 			beqz $t0,exit
 			
 			la	$a0,welcomemsg
-			jal	PrintString
+			jal	PrintString #calling fucntion to print string
 			la	$a0,doormsg
 			jal	PrintString
 			la	$a0,airmsg
@@ -43,17 +43,17 @@ addi $t4, $zero, 0
 			la	$a0,optionmsg
 			jal	PrintString
 			
-			li $v0,5
+			li $v0,5 #retrive input from user
 			syscall
-			move $t0,$v0
+			move $t0,$v0#move value from v0 to t0
 			
-			beq $t0,1,door
-			beq $t0,2,air
-			beq $t0,3,lamp
-
+			beq $t0,1,door # go to door fucntion
+			beq $t0,2,air # go to air fucntion
+			beq $t0,3,lamp # go to lamp fucntion
+			#comparing input 
 			
 			
-			j while
+			j while #jump to while until user enter 0
 			
 		
 		exit:
@@ -64,20 +64,22 @@ addi $t4, $zero, 0
 door:
 	la	$a0,dooropen
 	jal	PrintString
-	addi $t4, $t4, 1	
-	addi $t2,$zero,0
+	
+	addi $t4, $t4, 1	 #varibale for timer and stored it
+	addi $t2,$zero,0  #varible for duration the door stay open
+	
 	while1:
 		
 		
-		bgt $t2,9,exit1	
-		addi $t2,$t2,1	
-		li $v0,1
+		bgt $t2,9,exit1	#if duriation is more that 10s the door will close
+		addi $t2,$t2,1	#incrmenet 
+		
+		li $v0,1			
 		move $a0,$t2
 		syscall
-		
 		la	$a0,doortimer
 		jal	PrintString
-		
+		#print out integer as duriation+string as second "s"
 	
 		j while1
 		
@@ -87,33 +89,43 @@ door:
 	li $v0,1
 	move $a0,$t4
 	syscall
+	#print out door detail aka when the door is opened
 	
 	
-	
-	la	$a0,doorclose
+	la	$a0,doorclose #after 10s the door wil l close
 	jal	PrintString
 	j main
 
 
 air:
-	la	$a0,airon
-	jal	PrintString
-	j main
+	beqz $t5,purifier
+	beq $t5,1,nonpurifier
+	#checking the state of air
+	
+	purifier:
+		la	$a0,airon
+		jal	PrintString
+		li $t5,1	#load 1 as shows the air is good after air purifier is on
+		j main
+	
+	nonpurifier:
+		li $t5,0 #laod 0 as shows the air becoming bad when purifer is off
+		j main
 	
 
 lamp:
 
-	la	$a0,voicemsginput
+	la	$a0,voicemsginput 
 	jal	PrintString
-	
-	li $v0,5
+	li $v0,5 
 	syscall
+	#input ccommand for user
 	
 	move $t7,$v0
 	
 	beq $t7,1,on
 	beq $t7,0,off
-			
+	#if statement based on the command 
 
 	on:
 		la	$a0,voiceon
@@ -127,7 +139,7 @@ lamp:
 		
 
 
-PrintString:
+PrintString: # function to pritn out string
 	li	$v0,4
 	syscall
 	jr	$ra
